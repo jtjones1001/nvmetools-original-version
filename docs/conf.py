e4dbba5f95b5a5b621d131e6db3ea104dc51d1f3
Joseph Jones <jtjones1001@gmail.com>
2022-08-21 23:12:53 -0700

    first commit

---
+++ docs/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src'))
+
+autodoc_mock_imports = ['numpy','psutil','matplotlib','cycler','reportlab']
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'nvmetools'
+copyright = '2022, Joe Jones'
+author = 'Joe Jones'
+version = '0.2.0'
+release = version
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+autoclass_content = "init"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
